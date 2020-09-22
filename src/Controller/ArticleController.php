17469457e6d5da46715354a3bdb8989ac8c45c5a
Karim Samir <karim5977@gmail.com>
2020-09-22 17:42:49 +0200

    adding edit form

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -7,6 +7,11 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+// use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 
 class ArticleController extends Controller
 {
@@ -41,11 +46,94 @@ class ArticleController extends Controller
     //     $article->setbody("body " . $number);
 
     //     $entityManager->persist($article);
-        
+
     //     $entityManager->flush();
 
     //     return new Response("Save an article with id of " . $article->getId());
     // }
+
+
+    /**
+     * @Route("/article/new", name="article_new")
+     * @Method({"Get", "Post"})
+     */
+    public function new(Request $request)
+    {
+        $article = new Article();
+
+        $form = $this->createFormBuilder($article)
+            ->add("title", TextType::class, array("attr" =>
+            array("class" => "form-control")))
+            ->add("body", TextareaType::class, array(
+                "required" => false,
+                "attr" =>
+                array("class" => "form-control")
+            ))
+            ->add("save", SubmitType::class, array(
+                "label" => "Create",
+                "attr" =>
+                array("class" => "btn btn-primary mt-3")
+            ))
+            ->getForm();
+
+
+        $form->handleRequest($request);
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $article = $form->getData();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($article);
+
+            $entityManager->flush();
+
+            return $this->redirectToRoute("article_list");
+        }
+
+        return $this->render("articles/new.html.twig", array("form" => $form->createView()));
+    }
+
+/**
+     * @Route("/article/edit/{id}", name="article_edit")
+     * @Method({"Get", "Post"})
+     */
+    public function edit(Request $request, $id)
+    {
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+
+        $form = $this->createFormBuilder($article)
+            ->add("title", TextType::class, array("attr" =>
+            array("class" => "form-control")))
+            ->add("body", TextareaType::class, array(
+                "required" => false,
+                "attr" =>
+                array("class" => "form-control")
+            ))
+            ->add("save", SubmitType::class, array(
+                "label" => "Create",
+                "attr" =>
+                array("class" => "btn btn-primary mt-3")
+            ))
+            ->getForm();
+
+
+        $form->handleRequest($request);
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $article = $form->getData();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            // $entityManager->persist($article);
+
+            $entityManager->flush();
+
+            return $this->redirectToRoute("article_list");
+        }
+
+        return $this->render("articles/edit.html.twig", array("form" => $form->createView()));
+    }
 
 
     /**
@@ -57,5 +145,23 @@ class ArticleController extends Controller
         $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
 
         return $this->render("articles/show.html.twig", array("article" => $article));
+    }
+
+    /**
+     * @Route("/article/delete/{id}", name="article_delete")
+     * @Method({"Delete"})
+     */
+    public function delete($id)
+    {
+        
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+// die(var_dump($id));
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($article);
+
+        $entityManager->flush();
+
+        $response = new Response();
+        $response->send();
     }
 }
